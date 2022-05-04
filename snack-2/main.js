9424afa2bd59bf89fd359da07edf36c81b5899fa
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-04 11:56:56 +0200

    Corretto ordine risposta

--- snack-2/main.js
+++ snack-2/main.js
@@ -3,7 +3,7 @@ const parola2 = prompt(`Inserisci un'altra parola`);
 if (parola1.length == parola2.length) {
     alert(`Le due parole hanno lunghezza uguale`)
 } else if (parola1.length > parola2.length) {
-    alert(`${parola1} ${parola2}`)
-} else {
     alert(`${parola2} ${parola1}`)
+} else {
+    alert(`${parola1} ${parola2}`)
 }
