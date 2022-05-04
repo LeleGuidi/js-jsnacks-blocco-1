b933218578ae39cfb3eba2f2272e832dc1186b66
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-04 17:12:02 +0200

    Corretto snack-7

--- snack-7/main.js
+++ snack-7/main.js
@@ -2,6 +2,14 @@
 const listaNomi = [`Emanuele`, `Federico`, `Simone`]
 const listaCognomi = [`Guidi`, `Alpi`, `Miceli`]
 
+let listaUnica = [];
 //Creare un'unica lista di nomi e cognomi
-const listaUnica = [...listaNomi, ...listaCognomi]
-console.log(listaUnica)
+for (let i = 0; i < listaNomi.length; i++) {
+    const indiceNomi = Math.floor(Math.random() * listaNomi.length);
+    console.log(indiceNomi)
+    const indiceCognomi =  Math.floor(Math.random() * listaCognomi.length);
+    console.log(indiceCognomi)
+    listaUnica.push(`${listaNomi[indiceNomi]} ${listaCognomi[indiceCognomi]}`);
+    console.log(listaUnica)
+}
+console.log(listaUnica);
