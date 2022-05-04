d2cf087b85eefb45c89933cfd61c838d03b3e506
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-04 16:51:49 +0200

    Snack-7

---
+++ snack-7/main.js
@@ -0,0 +1,7 @@
+//Creare lista nomi e lista cognomi
+const listaNomi = [`Emanuele`, `Federico`, `Simone`]
+const listaCognomi = [`Guidi`, `Alpi`, `Miceli`]
+
+//Creare un'unica lista di nomi e cognomi
+const listaUnica = [...listaNomi, ...listaCognomi]
+console.log(listaUnica)
